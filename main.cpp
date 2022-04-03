c2216cba33364cefe369a9d46e1be4bc8d746280
Szymon Kulpinski <szymon.kulpinski@rwth-aachen.de>
2022-04-03 13:33:38 +0200

    Add GaussianBlur and Fix HSV bug

--- main.cpp
+++ main.cpp
@@ -5,16 +5,24 @@ using namespace cv;
 
 int main(int, char**) {
     cv::String imagePath = "/Users/szymon/Documents/coding/Projects/findTheBall2/Photos BBALL/IMG_1344_test.jpg";
-    cv::Mat image, imageHSV, outputImage, mask, image3;
+    cv::Mat src, blured, imageHSV, outputImage, mask, outputImageShow, eroded, dilated;
 
-    image = cv::imread(imagePath, cv::IMREAD_COLOR);
-    if (!image.data)
+    src = cv::imread(imagePath, cv::IMREAD_COLOR);
+    GaussianBlur(src,blured, Size(11, 11), 11, 11);
+    if (!src.data)
         throw std::invalid_argument("No image data, incorrect path!");
-    cv::cvtColor(image, imageHSV, cv::COLOR_BGR2HSV);
+    cv::cvtColor(blured, imageHSV, cv::COLOR_BGR2HSV);
 
-    int hmin = 7, hmax = 71; 
-    int smin = 48, smax = 158;
-    int vmin = 102, vmax = 255;
+    int hmin = 0, hmax = 13; 
+    int smin = 65, smax = 203;
+    int vmin = 1, vmax = 203;
+
+    // int hmin = 7, hmax = 71; 
+    // int smin = 48, smax = 158;
+    // int vmin = 102, vmax = 255;
+
+    // lower[2, 65, 1, 0]
+    // upper[13, 203, 203, 0]
 
     cv::String figureName = "Preview";
     namedWindow(figureName);
@@ -32,11 +40,12 @@ int main(int, char**) {
         cv::Scalar upper (hmax, smax, vmax);
         std::cout << "lower" << lower <<  std::endl;
         std::cout << "upper" << upper << std::endl;
-        inRange(image, lower, upper, outputImage);
+        inRange(imageHSV, lower, upper, outputImage);
+        cv::erode(outputImage, outputImage, getStructuringElement(MORPH_RECT, Size(5, 5)));
 
-        // Combine for images
-        cvtColor(outputImage, image3, COLOR_GRAY2BGR);
-        std::vector<cv::Mat> matrices = {image, image3};
+        // Combine images
+        cvtColor(outputImage, outputImageShow, COLOR_GRAY2BGR);
+        std::vector<cv::Mat> matrices = {src, blured, outputImageShow};
         cv::hconcat( matrices, mask );
 
         cv::imshow(figureName, mask);
