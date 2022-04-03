f41caf1cd737c3a5d43ef604941d39d645d342cd
Szymon Kulpinski <szymon.kulpinski@rwth-aachen.de>
2022-04-04 00:23:26 +0200

    Reformat code

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 #include <iostream>
 #include <filesystem>
 #include <unistd.h>
-using namespace cv; 
+using namespace cv;
 
-
-
-int main(int, char**) {
+int main(int, char **)
+{
     cv::Mat src, blured, imageHSV, outputImage, mask, outputImageShow, eroded, dilated;
 
     // Get Files
@@ -25,80 +24,69 @@ int main(int, char**) {
     fileList.push_back("IMG_1363.jpg");
     fileList.push_back("IMG_1364.jpg");
 
-
-    int hmin = 0, hmax = 12; 
+    int hmin = 0, hmax = 12;
     int smin = 126, smax = 255;
     int vmin = 1, vmax = 203;
-    cv::Scalar lower (hmin, smin, vmin);
-    cv::Scalar upper (hmax, smax, vmax);
-
+    cv::Scalar lower(hmin, smin, vmin);
+    cv::Scalar upper(hmax, smax, vmax);
 
     // Create Window
     cv::String figureName = "Preview";
     namedWindow(figureName, WINDOW_NORMAL);
-    resizeWindow(figureName, 600,600);
-
+    resizeWindow(figureName, 600, 600);
 
     for (int i = 0; i < fileList.size(); i++)
     {
-    cv::String imagePath = folderPath + fileList[i];
+        cv::String imagePath = folderPath + fileList[i];
 
-    src = cv::imread(imagePath, cv::IMREAD_COLOR);
-    if (!src.data)
-        throw std::invalid_argument("No image data, incorrect path!");
-    GaussianBlur(src,blured, Size(11, 11), 11, 11);
-    cv::cvtColor(blured, imageHSV, cv::COLOR_BGR2HSV);
+        src = cv::imread(imagePath, cv::IMREAD_COLOR);
+        if (!src.data)
+            throw std::invalid_argument("No image data, incorrect path!");
+        GaussianBlur(src, blured, Size(11, 11), 11, 11);
+        cv::cvtColor(blured, imageHSV, cv::COLOR_BGR2HSV);
 
-
-        // Thresholding
+        // Thresholding and contours
         inRange(imageHSV, lower, upper, outputImage);
         cv::erode(outputImage, outputImage, getStructuringElement(MORPH_RECT, Size(5, 5)));
         cv::dilate(outputImage, outputImage, getStructuringElement(MORPH_RECT, Size(10, 10)));
         std::vector<std::vector<cv::Point>> contours;
-        findContours(outputImage,contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+        findContours(outputImage, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
         Mat image_copy = src.clone();
 
-        std::vector<cv::Point2f>centers(contours.size());
-        std::vector<float>radius(contours.size());
-        std::vector<std::vector<cv::Point>> contoursPoly (contours.size());
+        std::vector<cv::Point2f> centers(contours.size());
+        std::vector<float> radius(contours.size());
+        std::vector<std::vector<cv::Point>> contoursPoly(contours.size());
         cv::String textPostion;
-        for (int i = 0; i < contours.size(); i++){
-                    approxPolyDP( contours[i], contoursPoly[i], 3, true);
-                    int area = contourArea(contours[i]);
-                    if (area < 50000) continue; 
-                    int areaPoly = contourArea(contoursPoly[i]);
+        for (int i = 0; i < contours.size(); i++)
+        {   
+            // Get smoothed area 
+            approxPolyDP(contours[i], contoursPoly[i], 3, true);
+            int area = contourArea(contours[i]);
+            if (area < 50000)
+                continue;
+            int areaPoly = contourArea(contoursPoly[i]);
 
-                    // Debugging 
-                    std::cout << "Area: " << area << std::endl;
-                    std::cout << "areaPoly: " << areaPoly << std::endl;
+            // Debugging
+            std::cout << "Area: " << area << std::endl;
+            std::cout << "areaPoly: " << areaPoly << std::endl;
 
-                    // Draw on image
-                    minEnclosingCircle( contoursPoly[i], centers[i], radius[i] );
-                    circle( image_copy, centers[i], (int)radius[i], Scalar(0, 255, 0), 3 );
-                    textPostion = (cv::String) ((std::string)"Ball center: " + std::to_string((int)centers[i].x) + (std::string)", " + std::to_string((int)centers[i].y)); // TODO: pretify
-                    putText(image_copy, textPostion, Point(40,40), FONT_HERSHEY_PLAIN, 3, Scalar(255, 255, 0),3);
-                    line(image_copy, Point(centers[i].x-20,centers[i].y), Point(centers[i].x+20,centers[i].y),Scalar(255, 255, 0),3);
-                    line(image_copy, Point(centers[i].x,centers[i].y-20), Point(centers[i].x,centers[i].y+20),Scalar(255, 255, 0),3);
+            // Draw on image
+            // minEnclosingCircle(contoursPoly[i], centers[i], radius[i]);
+            minEnclosingCircle(contours[i], centers[i], radius[i]);
+            circle(image_copy, centers[i], (int)radius[i], Scalar(0, 255, 0), 3);
+            textPostion = (cv::String)((std::string) "Ball center: " + std::to_string((int)centers[i].x) + (std::string) ", " + std::to_string((int)centers[i].y)); // TODO: pretify
+            putText(image_copy, textPostion, Point(40, 40), FONT_HERSHEY_PLAIN, 3, Scalar(255, 255, 0), 3);
+            line(image_copy, Point(centers[i].x - 20, centers[i].y), Point(centers[i].x + 20, centers[i].y), Scalar(255, 255, 0), 3);
+            line(image_copy, Point(centers[i].x, centers[i].y - 20), Point(centers[i].x, centers[i].y + 20), Scalar(255, 255, 0), 3);
         }
-
-
-
-
 
         // Combine images and print
         cvtColor(outputImage, outputImage, COLOR_GRAY2BGR);
         std::vector<cv::Mat> matrices = {src, image_copy};
-        cv::hconcat( matrices, mask );
+        cv::hconcat(matrices, mask);
         cv::imshow(figureName, mask);
         cv::waitKey(0);
-
     }
     return 0;
 }
-
-
-
-
-
-
