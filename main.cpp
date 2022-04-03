e0b8c0047f136f080f62dcfb9f6541289530e391
Szymon Kulpinski <szymon.kulpinski@rwth-aachen.de>
2022-04-03 12:25:39 +0200

    Display next to each other 2 images

--- main.cpp
+++ main.cpp
@@ -2,29 +2,31 @@
 #include <opencv2/opencv.hpp>
 using namespace cv; 
 
-// cv::Mat combineHorizontally(Mat matSrc, Mat matGray){
-//     Mat matDst(Size(matSrc.cols*2,matSrc.rows),matSrc.type(),Scalar::all(0));
-// Mat matRoi = matDst(Rect(0,0,matSrc.cols,matSrc.rows));
-// matSrc.copyTo(matRoi);
-// matRoi = matDst(Rect(matSrc.cols,0,matSrc.cols,matSrc.rows));
-// matGray.copyTo(matRoi);
-// }
+void combineHorizontally(Mat matSrc, Mat matGray){
+    Mat matDst(Size(matSrc.cols*2,matSrc.rows),matSrc.type(),Scalar::all(0));
+    Mat matRoi = matDst(Rect(0,0,matSrc.cols,matSrc.rows));
+    matSrc.copyTo(matRoi);
+    matRoi = matDst(Rect(matSrc.cols,0,matSrc.cols,matSrc.rows));
+    matGray.copyTo(matRoi);
+    imshow("result", matSrc);
+}
 
 
 int main(int, char**) {
     // cv::String imagePath = "/Users/szymon/Documents/coding/Projects/findTheBall2/usa.jpeg";
     // cv::String imagePath = "/Users/szymon/Documents/coding/Projects/findTheBall2/IMG_1317.jpg";
     cv::String imagePath = "/Users/szymon/Documents/coding/Projects/findTheBall2/Photos BBALL/IMG_1344_test.jpg";
-    cv::Mat imageHSV, outputImage, mask;
+    cv::Mat imageHSV, outputImage, mask, image3;
     // cv::Mat outputImage;
     cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+    cv::Mat image2 = cv::imread(imagePath, cv::IMREAD_COLOR);
+
     if (!image.data)
         throw std::invalid_argument("No image data, incorrect path!");
     cv::cvtColor(image, imageHSV, cv::COLOR_BGR2HSV);
     // cv::cvtColor(image, imageHSV, cv::COLOR_BGR2GRAY);
 
-    // std::vector<cv::Mat> matrices = {image, imageHSV};
-    // cv::hconcat( matrices, outputImage );
+
 
     // hconcat(image,imageHSV,outputImage);
     // int hmin = 0, hmax = 19; 
@@ -65,7 +67,21 @@ int main(int, char**) {
 
         inRange(image, lower, upper, outputImage);
 
-        // cv::Canny(outputImage,image);
+        // image.convertTo(image, CV_8U, 1, 0);
+        cvtColor(outputImage, image3, COLOR_GRAY2BGR);
+        
+
+        std::cout  << image.type() << std::endl;
+        std::cout  << outputImage.type()  << std::endl;
+        std::cout  << image3.type()  << std::endl;
+
+
+
+        std::vector<cv::Mat> matrices = {image, image3};
+        cv::hconcat( matrices, mask );
+
+        cv::imshow("mask",mask);
+        // combineHorizontally(image, outputImage);
         // cv::imshow("imageHSV", imageHSV);
         cv::imshow("image", image);
         cv::imshow("outputImage", outputImage);
