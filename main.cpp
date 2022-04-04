28e97d40338daaa51bb3219100b8e38c442dd0bc
Szymon Kulpinski <szymon.kulpinski@rwth-aachen.de>
2022-04-04 18:11:00 +0200

    Revert "Try sth out"
    This reverts commit 43146779bb4d7ae5960cb90869f7e628ba39ac7d.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
-#include "main.hpp"
+#include <opencv2/opencv.hpp>
+
+static const cv::Scalar greenColor = cv::Scalar(0, 255, 0);
+static const cv::Scalar turquoiseColor = cv::Scalar(255, 255, 0);
+static const int hmin = 0, hmax = 12; 
+static const int smin = 126, smax = 255;
+static const int vmin = 1, vmax = 203;
+static const cv::Scalar lower(hmin, smin, vmin);
+static const cv::Scalar upper(hmax, smax, vmax);
+static const int minBallArea = 50000;
+
 
 cv::String getDescription(const cv::Point2f& centers)
 {
